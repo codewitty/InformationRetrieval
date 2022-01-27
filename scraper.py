60d588e8fdfe724594af54ba329ea95a92897863
Jason Yang <54388343+Pesra@users.noreply.github.com>
2022-01-26 21:56:51 -0800

    My current version of Scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag
 from bs4 import BeautifulSoup
+
 
 
 def scraper(url, resp):
@@ -12,7 +13,7 @@ def scraper(url, resp):
 
 def extract_next_links(url, resp):
     
-    # Implementation required here.
+    # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
@@ -25,17 +26,19 @@ def extract_next_links(url, resp):
     # 2022-01-26 01:41:03,044 - Worker-0 - INFO - Downloaded http://www.informatics.uci.edu/files/pdf/InformaticsBrochure-March2018, status <200>, using cache ('styx.ics.uci.edu', 9005).
     
     
-    print(f'THIS IS A {resp.error}')
-    l = list()
+    #print(f'THIS IS A {resp.error}')
+    print()
+    s = set()
     if resp.status/100 == 2:
-        print("2")
         soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-        print("3")
-        #urls = []
         for link in soup.find_all('a'):
-            #if is_valid(link):
-            l.append(str(link.get('href')))
-    return l
+            if link.get('href') != None:
+                unfragmented = link.get('href').split('#')[0]
+            
+                s.add(unfragmented)
+                              
+            #l.append(str(link.get('href')))
+    return list(s)
     
 
 
@@ -43,11 +46,18 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-    if ('.ics.uci.edu/' in url) or ('.cs.uci.edu/' in url) or ('.informatics.uci.edu/' in url) or ('.stat.uci.edu/' in url) or ('today.uci.edu/department/information_computer_sciences/' in url):
-        
-        try:
-            parsed = urlparse(url)
-            if parsed.scheme not in set(["http", "https"]) or ('files/' in url):
+    
+    try:
+        parsed = urlparse(url)
+        if ('.ics.uci.edu/' in url) or ('.cs.uci.edu/' in url) or ('.informatics.uci.edu/' in url) or ('.stat.uci.edu/' in url) or ('today.uci.edu/department/information_computer_sciences/' in url):
+            
+            invalid_set = {'wics.ics.uci.edu/events', 'wics.ics.uci.edu/week', 'wics.ics.uci.edu/wics', 'evoke.ics.uci.edu/about/'}
+            
+            for invalid in invalid_set:
+                if invalid in url:
+                    return False
+            
+            if parsed.scheme not in set(["http", "https"]) or ('files/' in url) or ('pdf/' in url) or ('.pdf' in url):
                 return False
             return not re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -57,10 +67,10 @@ def is_valid(url):
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
                 + r"|thmx|mso|arff|rtf|jar|csv"
-                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz|flac|txt|py)$", parsed.path.lower())
+        else:
+            return False
 
-        except TypeError:
-            print ("TypeError for ", parsed)
-            raise
-    else:
-        return False
+    except TypeError:
+        print ("TypeError for ", parsed)
+        raise
