4dd5477e0c1a0b61a44b200d3c81024f64e6e477
Jason Yang <54388343+Pesra@users.noreply.github.com>
2022-01-30 21:44:27 -0800

    accumulate info

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 
 
+
 tokenMap = {}
 
 stop_words=["a","about","above","after","again","against","all","am","an","and","any","are","arent","as","at","be","because","been","before","being","below","between","both","but","by","cant","cannot","could","couldnt","did","didnt","do","does","doesnt","doing","dont","down","during","each","few","for","from","further","had","hadnt","has","hasnt","have","havent","having","he","hed","hell","hes","her","here","heres","hers","herself","him","himself","his","how","hows","i","id","ill","im","ive","if","in","into","is","isnt","it","its","its","itself","lets","me","more","most","mustnt","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shant","she","shed","shell","shes","should","shouldnt","so","some","such","than","that","thats","the","their","theirs","them","themselves","then","there","theres","these","they","theyd","theyll","theyre","theyve","this","those","through","to","too","under","until","up","very","was","wasnt","we","wed","well","were","weve","were","werent","what","whats","when","whens","where","wheres","which","while","who","whos","whom","why","whys","with","wont","would","wouldnt","you","youd","youll","youre","youve","your","yours","yourself","yourselves"]
@@ -100,7 +101,7 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     
-    # 2022-01-26 01:41:03,044 - Worker-0 - INFO - Downloaded http://www.informatics.uci.edu/files/pdf/InformaticsBrochure-March2018, status <200>, using cache ('styx.ics.uci.edu', 9005).
+
     
     #/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
 
@@ -108,6 +109,7 @@ def extract_next_links(url, resp):
     if resp.status/100 == 2:
         soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
         for link in soup.find_all('a'):
+                
             unfragmented = link.get('href')
             if unfragmented != None:
                 unparsed = urlparse(unfragmented)
@@ -121,17 +123,25 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    
+    # if urllib.request.urlopen(url).getcode() != 200:
+    #     print(urllib.request.urlopen(url).getcode())
+    #     return False
+    
+    # http = httplib2.Http()
+    # status, response = http.request(url)
+    #print(status)
+    
     if (('.ics.uci.edu/' in url) or ('.cs.uci.edu/' in url) or ('.informatics.uci.edu/' in url) or ('.stat.uci.edu/' in url) or ('today.uci.edu/department/information_computer_sciences/' in url)) and not(re.findall(r"\d{4}-\d{2}", url)):
         try:
             parsed = urlparse(url)
-            # Filter for valid urls, remove duplicate pages from Ramesh's blog
             if parsed.scheme not in set(["http", "https"]) or ('files/' in url) or ('img' in url) or ('ramesh/page' in url):
                 return False
 
             return not re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
-                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|php|odc"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|php|odc|diff"
                 + r"|ps|eps|tex|ppt|pptx|ppsx|doc|docx|xls|xlsx|names"
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
